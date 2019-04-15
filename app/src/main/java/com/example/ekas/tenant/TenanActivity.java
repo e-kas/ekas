82fc20b8f8fb93789828a8d6c1a17b6dd37705bd
Majidil Khithar <nuralimajids@gmail.com>
2019-04-15 21:50:14 +0700

    tambah tenan

---
+++ app/src/main/java/com/example/ekas/tenant/TenanActivity.java
@@ -0,0 +1,18 @@
+package com.example.ekas.tenant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ekas.R;
+
+public class TenanActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenan);
+    }
+}
